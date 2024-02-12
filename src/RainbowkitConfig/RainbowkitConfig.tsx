628e2c66fa6711505170a59a1c7a06b509424f32
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-12 15:33:28 +0500

    fix: change chain from goerli to sepolia

--- src/RainbowkitConfig/RainbowkitConfig.tsx
+++ src/RainbowkitConfig/RainbowkitConfig.tsx
@@ -1,15 +1,15 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { goerli } from "viem/chains";
+import { sepolia } from "viem/chains";
 import { env_Vars } from "../config/env";
-import {  http } from "wagmi";
+import { http } from "wagmi";
 
 export const config = getDefaultConfig({
   appName: "Rock Paper Scissors Lizard Spock",
   projectId: env_Vars.WALLET_CONNECT_PROJECT_ID,
-  chains: [goerli],
+  chains: [sepolia],
   transports: {
-    [goerli.id]: http(env_Vars.INFURA_ENDPOINT),
+    [sepolia.id]: http(env_Vars.INFURA_ENDPOINT),
   },
 });
