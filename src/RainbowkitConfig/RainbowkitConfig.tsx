a8e7bb1dec482c7bbbd5ae4bcf73277782ba801d
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-13 09:04:32 +0500

    refactor: refactor wagmiConfig

--- src/RainbowkitConfig/RainbowkitConfig.tsx
+++ src/RainbowkitConfig/RainbowkitConfig.tsx
@@ -3,13 +3,16 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { sepolia } from "viem/chains";
 import { env_Vars } from "../config/env";
-import { http } from "wagmi";
+import {  http } from "wagmi";
 
-export const wagmiConfig = getDefaultConfig({
+const SELECTED_CHAIN = sepolia
+const transports = {
+  [SELECTED_CHAIN.id]: http(env_Vars.INFURA_ENDPOINT),
+} as const;
+
+export const wagmiProviderConfig = getDefaultConfig({
   appName: "Rock Paper Scissors Lizard Spock",
   projectId: env_Vars.WALLET_CONNECT_PROJECT_ID,
-  chains: [sepolia],
-  transports: {
-    [sepolia.id]: http(env_Vars.INFURA_ENDPOINT),
-  },
+  chains: [SELECTED_CHAIN],
+  transports,
 });
