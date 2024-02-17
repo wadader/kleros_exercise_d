6e721c66ee88befdffbcefe5f1e6ca873e888add
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-02-17 12:12:21 +0500

    feat: add store

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-import { EthAddress } from "../types/identifier";
+import type { EthAddress } from "../types/identifier";
+import { Move } from "../types/game";
 
 const useGameStore = create<GameStore>()(
   persist(
@@ -10,20 +11,29 @@ const useGameStore = create<GameStore>()(
         salt: undefined,
         identifier: undefined,
         selectedContract: undefined,
+        creatorMove: Move.Null,
       },
       setters: {
-        salt: (newSalt) =>
+        salt: (newSalt) => {
           set((state) => {
             state.values.salt = newSalt;
-          }),
-        identifier: (newIdentifier) =>
+          });
+        },
+        identifier: (newIdentifier) => {
           set((state) => {
             state.values.identifier = newIdentifier;
-          }),
-        selectedContract: (newSelectedContract) =>
+          });
+        },
+        selectedContract: (newSelectedContract) => {
           set((state) => {
             state.values.selectedContract = newSelectedContract;
-          }),
+          });
+        },
+        creatorMove: (selectedMove) => {
+          set((state) => {
+            state.values.creatorMove = selectedMove;
+          });
+        },
       },
     })),
     { name: "game-storage", partialize: (state) => ({ values: state.values }) }
@@ -37,10 +47,12 @@ interface GameStore {
     salt: string | undefined;
     identifier: string | undefined;
     selectedContract: EthAddress | undefined;
+    creatorMove: Move;
   };
   setters: {
     salt: (newSalt: string) => void;
     identifier: (newIdentifier: string) => void;
     selectedContract: (newSelectedContract: EthAddress) => void;
+    creatorMove: (move: Move) => void;
   };
 }
